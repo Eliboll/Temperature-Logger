6ec80a7185fd8c813cad9f9a0535bfe8e3267c69
Eliboll <40479800+Eliboll@users.noreply.github.com>
2021-09-02 17:56:33 -0500

    Add files via upload

---
+++ templog.ino
@@ -0,0 +1,37 @@
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+#include "Adafruit_Si7021.h"
+Adafruit_Si7021 sensor = Adafruit_Si7021();
+const char* ssid     = "Calcifer";
+const char* password = "~FatDogMan27Tacos~";
+
+void setup() {
+  // put your setup code here, to run once:
+  sensor.begin();
+  
+  WiFi.begin(ssid,password);
+  while (WiFi.status() != WL_CONNECTED ) {delay(500);} // forces the board to wait until the wifi is connected
+}
+
+void loop() {
+ 
+  if (WiFi.status() == WL_CONNECTED) { //Check WiFi connection status
+ 
+    HTTPClient http;  //Declare an object of class HTTPClient
+    float temp = (sensor.readTemperature() * 1.8) +32;
+    http.begin("192.168.1.203:5000/?temp=" + String(temp));  //Specify request destination
+    int httpCode = http.GET();                                  //Send the request
+ 
+    if (httpCode > 0) { //Check the returning code
+ 
+      String payload = http.getString();   //Get the request response payload
+      Serial.println(payload);             //Print the response payload
+ 
+    }
+ 
+    http.end();   //Close connection
+ 
+  }
+ 
+  delay(3000);    //Send a request every 30 seconds
+}
